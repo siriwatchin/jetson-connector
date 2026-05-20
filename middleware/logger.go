7e3faea7b124a8eef3c79eda16ff15800dcb5f5b
siriwatchin.dev <siriwat.chin@gmail.com>
2026-05-20 10:22:45 +0700

    enhance logger middleware to capture request body

--- middleware/logger.go
+++ middleware/logger.go
@@ -1,6 +1,8 @@
 package middleware
 
 import (
+	"bytes"
+	"io"
 	"log/slog"
 	"time"
 
@@ -9,14 +11,22 @@ import (
 
 func Logger() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var body []byte
+		if c.Request.Body != nil {
+			body, _ = io.ReadAll(c.Request.Body)
+			c.Request.Body = io.NopCloser(bytes.NewBuffer(body))
+		}
+
 		start := time.Now()
 		c.Next()
+
 		slog.Info("request",
 			"method", c.Request.Method,
 			"path", c.Request.URL.Path,
 			"status", c.Writer.Status(),
 			"latency", time.Since(start).String(),
 			"ip", c.ClientIP(),
+			"body", string(body),
 		)
 	}
 }

bddac46c69912cc0951adf00c2f63d7226acecaa
siriwatchin.dev <siriwat.chin@gmail.com>
2026-05-21 16:21:35 +0700

    remove unused fields from FailedLog struct in model package

--- model/failed_log.go
+++ model/failed_log.go
@@ -2,16 +2,11 @@ package model
 
 import (
 	"encoding/json"
-	"time"
-
-	"github.com/google/uuid"
 )
 
 type FailedLog struct {
-	ID        uuid.UUID       `gorm:"column:id"`
-	CreatedAt time.Time       `gorm:"column:created_at"`
-	Payload   json.RawMessage `gorm:"column:payload;type:jsonb"`
-	Reason    string          `gorm:"column:reason"`
+	Payload json.RawMessage `gorm:"column:payload;type:jsonb"`
+	Reason  string          `gorm:"column:reason"`
 }
 
 func (FailedLog) TableName() string {
